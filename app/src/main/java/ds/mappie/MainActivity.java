046c5c62c954ab53a0af4361114919ba2a91e688
George Metakos <p3130132@dias.aueb.gr>
2016-06-09 00:14:21 +0300

    commit

--- app/src/main/java/ds/mappie/MainActivity.java
+++ app/src/main/java/ds/mappie/MainActivity.java
@@ -1,11 +1,13 @@
 package ds.mappie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,14 +18,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                showMaps();
             }
         });
+    }
+
+    public void showMaps(){
+        Intent k = new Intent(this, MapsActivity.class);
+        startActivity(k);
     }
 
 }
